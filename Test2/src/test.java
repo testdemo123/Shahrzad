903c90159c46c771b15f5251aaded5dcc7716a4e
testdemo123 <sh.rafatnia@gmail.com>
2013-01-31 15:59:14 -0700

    third change

--- Test2/src/test.java
+++ Test2/src/test.java
@@ -7,7 +7,6 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("change");
 		System.out.println("bye");
 
 	}
