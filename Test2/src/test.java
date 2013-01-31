70ca3cf4838f279e36d6ad0e8351e567a8c94df6
Chenlei Zhang <rayzhangcl@gmail.com>
2013-01-31 15:57:52 -0700

    commit 2

--- Test2/src/test.java
+++ Test2/src/test.java
@@ -7,7 +7,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("change");
+		System.out.println("change by chenlei");
 		System.out.println("bye");
 
 	}
