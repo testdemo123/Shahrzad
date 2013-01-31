26259e3d9be395466582ad4b23c0c63d42ce3d75
testdemo123 <sh.rafatnia@gmail.com>
2013-01-31 15:53:55 -0700

    second commit from shahrzad

--- Test2/src/test.java
+++ Test2/src/test.java
@@ -7,7 +7,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Hello from Chenlei");
+		System.out.println("change");
 		System.out.println("bye");
 
 	}
